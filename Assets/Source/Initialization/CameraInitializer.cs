4b427f998df609ce94204026b59e207b9dd81558
AtennopVer <anton7185569@gmail.com>
2024-01-10 20:56:39 +0300

    Made test canvas for target and some initialization scripts

---
+++ Assets/Source/Initialization/CameraInitializer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Vuforia;
+
+namespace Weapons.Initialization
+{
+    public sealed class CameraInitializer : MonoBehaviour
+    {
+        private void Awake()
+        {
+            VuforiaApplication.Instance.Initialize(FusionProviderOption.PREFER_PLATFORM_FUSION_PROVIDER);
+            VuforiaApplication.Instance.OnVuforiaStarted += InitAR;
+        }
+
+        private void InitAR() 
+            => VuforiaBehaviour.Instance.CameraDevice.SetFocusMode(FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+}
