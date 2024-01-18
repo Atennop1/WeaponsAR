d51db37074e0323adb78292ba14b370d4f99999f
AtennopVer <anton7185569@gmail.com>
2024-01-18 22:10:36 +0300

    Made menu

---
+++ Assets/Source/UI/ExitGameButton.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Weapons.UI
+{
+    public sealed class ExitGameButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+            
+        private void Awake() 
+            => _button.onClick.AddListener(Exit);
+
+        private void OnDestroy() 
+            => _button.onClick.RemoveListener(Exit);
+
+        private void Exit()
+            => Application.Quit();
+    }
+}
---
+++ Assets/Source/UI/LoadSceneButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Weapons.UI
+{
+    public sealed class LoadSceneButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        [SerializeField] private int _sceneIndex;
+            
+        private void Awake() 
+            => _button.onClick.AddListener(LoadScene);
+
+        private void OnDestroy() 
+            => _button.onClick.RemoveListener(LoadScene);
+
+        private void LoadScene()
+            => SceneManager.LoadScene(_sceneIndex);
+    }
+}
