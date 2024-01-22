33337db1c7848dee9c5fd3e44cfccca744baf881
AtennopVer <anton7185569@gmail.com>
2024-01-22 21:58:54 +0300

    Fixes with flashlight and on scene

---
+++ Assets/Source/Buttons/LoadFlashlightButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+using Weapons.Camera;
+using Weapons.Saving;
+using Weapons.Saving.Paths;
+
+namespace Weapons.Buttons
+{
+    public sealed class LoadFlashlightButton : MonoBehaviour
+    {
+        [SerializeField] private Button _button;
+        
+        private ISaveStorage<FlashlightState> _stateStorage;
+            
+        private void Awake()
+        {
+            _stateStorage = new JsonStorage<FlashlightState>(new Path("Flashlight.json"));
+            _button.onClick.AddListener(Load);
+        }
+
+        private void OnDestroy() 
+            => _button.onClick.RemoveListener(Load);
+
+        private void Load()
+        {
+            if (VuforiaBehaviour.Instance != null)
+                VuforiaBehaviour.Instance.CameraDevice.SetFlash(_stateStorage.HasSave() && _stateStorage.Load().Value);
+        }
+    }
+}
